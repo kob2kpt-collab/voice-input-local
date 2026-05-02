ebae747f756010212056b43fe764504eb764465a
Павел <kob.2k.pt@gmail.com>
2026-05-03 00:58:14 +0300

    Fix window bounds and settings layout

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from voice_input_app.ui_window_constraints import apply_patches
+
+apply_patches()
+
 from voice_input_app.ui import run
 
 raise SystemExit(run())
--- voice_input_app/__main__.py
+++ voice_input_app/__main__.py
@@ -1,3 +1,7 @@
+from .ui_window_constraints import apply_patches
+
+apply_patches()
+
 from .ui import run
 
 raise SystemExit(run())
